eb47db89c35f38b5c329f213fb7bfbbd9be8b0af
littlethree <jiarao@gmail.com>
2016-11-22 12:45:18 +0800

    创建项目

---
+++ OCDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OCDemo
+//
+//  Created by RaoJia on 2016/11/22.
+//  Copyright © 2016年 RaoJia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
